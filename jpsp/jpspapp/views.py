62a80e27977ba7f86e46c418c60bab370823f7af
qiushimao <qiushimao@hotmail.com>
2017-04-01 11:15:17 +0800

    update /templates/html add Bootstrap

--- jpsp/jpspapp/views.py
+++ jpsp/jpspapp/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("A website.")
+def club_fileupload(request):
+    return HttpResponse("file")
