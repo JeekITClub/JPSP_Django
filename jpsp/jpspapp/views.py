51f5dbfe951436d77c94caa540c7f9fbffab8a14
qiushimao <qiushimao@hotmail.com>
2017-04-01 17:45:57 +0800

    update

--- jpsp/jpspapp/views.py
+++ jpsp/jpspapp/views.py
@@ -9,5 +9,16 @@ def club_fileupload(request):
 def admin_login(request):
     template=loader.get_template('admin/login.html')
     content={
+
     }
     return HttpResponse(template.render(content,request))
+
+def login(request):
+    template=loader.get_template('')
+    content={
+
+    }
+    return HttpResponse(template.render(content,request))
+
+
+
