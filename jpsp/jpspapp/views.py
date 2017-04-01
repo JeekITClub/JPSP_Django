6f495801dee379b124dd5b36642cc7977a0d360f
qiushimao <qiushimao@hotmail.com>
2017-04-01 21:58:11 +0800

    Problems in file post and handle

--- jpsp/jpspapp/views.py
+++ jpsp/jpspapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.template import loader
+from .forms import UploadFileForm
 # Create your views here.
 def index(request):
     template=loader.get_template('index.html')
@@ -18,7 +19,21 @@ def login(request):
     template=loader.get_template('login.html')
     content={}
     return HttpResponse(template.render(content,request))
-
+def club_admin_file_upload(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        def handle_uploaded_file(f):
+            with open('files', 'wb+') as destination:
+                for chunk in f.chunks():
+                    destination.write(chunk)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+        template=loader.get_template('login.html')
+        content={}
+        return HttpResponse(template.render(content,request))
 # TODO: file request and save
 
 
