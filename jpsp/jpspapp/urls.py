49d304cea0bc1e4bb35357516b0512179cfd932a
qiushimao <qiushimao@hotmail.com>
2017-04-02 21:25:17 +0800

    Club_admin_file_upload is ok.

--- jpsp/jpspapp/urls.py
+++ jpsp/jpspapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^admin/login',views.admin_login,name="login"),
+    url(r'^club/admin/file/success',views.club_admin_file_upload,name="club_admin_file_upload"),
     url(r'^club/admin/file',views.club_admin_file,name="club_admin_file"),
-    url(r'^club/admin/file/success',views.club_admin_file_upload,name="club_admin_file_upload")
 ]
--- jpsp/jpspapp/views.py
+++ jpsp/jpspapp/views.py
@@ -1,40 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
-from .forms import UploadFileForm
+import os
+
 # Create your views here.
 def index(request):
-    template=loader.get_template('index.html')
-    content={}
-    return HttpResponse(template.render(content,request))
+    template = loader.get_template('index.html')
+    content = {}
+    return HttpResponse(template.render(content, request))
+
+
 def club_admin_file(request):
-    template=loader.get_template('club/admin/file.html')
-    content={}
-    return HttpResponse(template.render(content,request))
+    template = loader.get_template('club/admin/file.html')
+    content = {}
+    return HttpResponse(template.render(content, request))
+
+
 def admin_login(request):
-    template=loader.get_template('admin/login.html')
-    content={}
-    return HttpResponse(template.render(content,request))
+    template = loader.get_template('admin/login.html')
+    content = {}
+    return HttpResponse(template.render(content, request))
+
+
 def login(request):
-    template=loader.get_template('login.html')
-    content={}
-    return HttpResponse(template.render(content,request))
+    template = loader.get_template('login.html')
+    content = {}
+    return HttpResponse(template.render(content, request))
+
+
 def club_admin_file_upload(request):
     if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+        file=request.FILES.get("file",None)
         def handle_uploaded_file(f):
-            with open('files', 'wb+') as destination:
-                for chunk in f.chunks():
-                    destination.write(chunk)
-        if form.is_valid():
+            destination=open(os.path.join(r"D:/upload",f.name),'wb+')
+            for chunk in file.chunks():
+                destination.write(chunk)
+            destination.close()
+        if file:
             handle_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
+            return HttpResponse("hello")
+        else:
+            return HttpResponse("no file for upload!")
     else:
-        form = UploadFileForm()
-        template=loader.get_template('login.html')
-        content={}
-        return HttpResponse(template.render(content,request))
-# TODO: file request and save
+        template = loader.get_template('login.html')
+        content = {}
+        return HttpResponse("sorry")
 
 
-
+# TODO: Finish the club_admin_file_upload
