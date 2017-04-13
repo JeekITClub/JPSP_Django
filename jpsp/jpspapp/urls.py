181ddee20e6db2715ed5a443a490eecb1bf28168
nichujie <ncj19991213@126.com>
2017-04-13 22:42:35 +0800

    修改错别字

--- jpsp/jpspapp/urls.py
+++ jpsp/jpspapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^login/',views.login,name='login'),
     url(r'^admin/',views.admin,name="admin"),
     url(r'^admin/login',views.admin_login,name="admin_login"),
     url(r'^club/admin/file/success',views.club_admin_file_upload,name="club_admin_file_upload"),
