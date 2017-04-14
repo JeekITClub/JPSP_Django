9a766c9abfdb45d1f76a3ef88259e1526a13cf05
qiushimao <qiushimao@hotmail.com>
2017-04-14 20:59:45 +0800

    add dockerfile

--- jpsp/jpspapp/urls.py
+++ jpsp/jpspapp/urls.py
@@ -5,10 +5,11 @@ from . import views
 urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^login/',views.login,name='login'),
+    url(r'^admin/news/overview/',views.admin_news_check,name="admin_news_overview"),
+    url(r'^admin/login',views.admin_login,name="admin_login"),
     url(r'^admin/',views.admin,name='admin'),
     url(r'^register/',views.register,name='register'),
-    url(r'^admin/login',views.admin_login,name="admin_login"),
     url(r'^club/admin/file/success',views.club_admin_file_upload,name="club_admin_file_upload"),
     url(r'^club/admin/file',views.club_admin_file,name="club_admin_file"),
-    url(r'test/',views.test,name='test'),
+    url(r'^test/',views.test,name='test'),
 ]
