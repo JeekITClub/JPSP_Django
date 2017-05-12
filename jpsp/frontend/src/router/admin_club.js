a0275da23cd6e7c0550c0d3bc574a8b083ba6e06
qiushimao <qiushimao@hotmail.com>
2017-05-12 19:52:18 +0800

    init frontend

--- jpsp/frontend/src/router/admin_club.js
+++ jpsp/frontend/src/router/admin_club.js
@@ -1,6 +1,9 @@
+/**
+ * Created by wto on 2017/5/12 0012.
+ */
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/pages/index/index'
+import index from '@/pages/admin_club/index'
 
 Vue.use(Router)
 
