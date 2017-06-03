edb0cedc7f5ec212a1f20ad1e3548fed00b587dc
qiushimao <qiushimao@hotmail.com>
2017-06-03 23:05:30 +0800

    add About.vue

--- jpsp/frontend/src/components/admin_club/JNavTop.vue
+++ jpsp/frontend/src/components/admin_club/JNavTop.vue
@@ -7,12 +7,12 @@
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
           <li><router-link to="#">主页</router-link></li>
-          <li><router-link to="#">关于我们</router-link></li>
+          <li><router-link to="about">关于</router-link></li>
           <li><router-link to="#">联系我们</router-link></li>
         </ul>
         <ul class="nav navbar-nav navbar-right" v-if="Authenticate===true">
           <li><a href="../">{{ ClubName }}</a></li>
-          <li><router-link to="/profile"></router-link>资料修改</li>
+          <li><router-link to="profile"></router-link>资料修改</li>
           <li><a href="/">消息列表</a></li>
           <li><a href="/">注销</a></li>
         </ul>
