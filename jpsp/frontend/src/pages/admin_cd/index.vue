150063bcf1525c4343ef6df9245d3576d756a0e6
qiushimao <qiushimao@hotmail.com>
2017-06-08 09:48:36 +0800

    update admin_cd/index.vue

--- jpsp/frontend/src/pages/admin_cd/index.vue
+++ jpsp/frontend/src/pages/admin_cd/index.vue
@@ -2,7 +2,7 @@
   <div class="LoginForm">
     <el-row class="tac">
       <el-col :span="8" :offset="8">
-        <el-form ref="LoginForm" :model="LoginForm" v-if="Authenticate===false">
+        <el-form ref="LoginForm" :model="LoginForm" v-if="Authenticate===false || Authenticate===null">
           <el-form-item label="学号" :required=true>
             <el-input v-model="LoginForm.UserName" placeholder="社团部账号" autofocus=""></el-input>
           </el-form-item>
@@ -14,7 +14,7 @@
             <el-button type="primary" @click="onSubmit">登陆</el-button>
           </el-form-item>
           <el-form-item>
-            <p v-if="settings.NotAuthenticated===true">登陆失败</p>
+            <p v-if="Authenticate===true">登陆失败</p>
           </el-form-item>
         </el-form>
         <h1 v-if="Authenticate===true">已登录</h1>
@@ -30,9 +30,6 @@
         LoginForm: {
           UserName: '',
           Password: ''
-        },
-        settings: {
-          NotAuthenticated: false
         }
       }
     },
@@ -40,18 +37,18 @@
       onSubmit () {
         axios({
           method: 'POST',
-          url: '',
+          url: 'http://127.0.0.1:8000/api/login',
           data: JSON.stringify({
             UserName: this.LoginForm.UserName,
             Password: this.LoginForm.Password
           })
         }).then(function (response) {
           if (response.data.message === 'User Authenticated') {
-            this.$store.commit('Authenticate',true)
+            this.$store.commit('Authenticate', true)
             this.$store.commit('ApplyUserName', this.UserName)
-            this.$store.commit('ApplyToken',response.data.Token)
+            this.$store.commit('ApplyToken', response.data.Token)
           } else if (response.data.message === 'User Not Authenticated') {
-            this.data.settings.NotAuthenticated = true
+            this.$store.commit('Authenticate', false)
           }
         }.bind(this))
           .catch(function (error) {
