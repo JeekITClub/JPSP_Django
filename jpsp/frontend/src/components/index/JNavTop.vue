af67afa5130cb6c4784164d1d29bbefbec126869
Ni_Chujie <ncj19991213@126.com>
2017-06-09 21:59:24 +0800

    Edit NavTop

--- jpsp/frontend/src/components/index/JNavTop.vue
+++ jpsp/frontend/src/components/index/JNavTop.vue
@@ -6,15 +6,22 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-xs-4">
-            <div id="fh5co-logo"><a href="index.html">建平中学学生平台</a></div>
+            <div id="fh5co-logo"><a href="" class="title">建平中学学生平台</a></div>
           </div>
           <div class="col-xs-8 text-right menu-1">
             <ul>
-              <li class="active"><a href="index.html">首页</a></li>
-              <li><a href="product.html">社团动态</a></li>
-              <li class="has-dropdown"><a href="blog.html">社团介绍</a></li>
+              <li class="active"><a href="">首页</a></li>
+              <li class="has-dropdown"><a href="">社团</a>
+              <ul class="dropdown">
+                <li><a href="#">社团列表</a></li>
+								<li><a href="#">社团动态</a></li>
+								<li><a href="#">社团活动</a></li>
+							</ul>
+              </li>
+              <li><a href="product.html">失物招领</a></li>
               <li><a href="about.html">关于</a></li>
-              <li class="btn-cta"><a href="#"><span>登录</span></a></li>
+              <li class="btn-cta" v-if="Authenticate===false || Authenticate===null"><a href="#"><span>登录</span></a></li>
+              <li class="btn-cta" v-else><a href="#"><span>注销</span></a></li>
             </ul>
           </div>
         </div>
@@ -27,13 +34,16 @@
   export default {
     data () {
       return {
-        activeIndex: '1',
-        activeIndex2: '1'
       }
     },
     methods: {
       handleSelect (key, keyPath) {
         console.log(key, keyPath)
+      }
+    },
+    computed: {
+      Authenticate () {
+        return this.$store.state.Authenticated
       }
     }
   }
--- jpsp/frontend/src/pages/index/index.vue
+++ jpsp/frontend/src/pages/index/index.vue
@@ -255,33 +255,18 @@
     },
     methods: {
 
+    },
+    computed: {
+      Authenticate () {
+        return this.$store.state.Authenticated
+      }
     }
   }
 </script>
 <style scoped>
-  @import url("//unpkg.com/element-ui@1.3.2/lib/theme-default/index.css");
   @import '../../assets/index/css/animate.css';
   @import '../../assets/index/css/icomoon.css';
   @import '../../assets/index/css/magnific-popup.css';
   @import '../../assets/index/css/flexslider.css';
   @import '../../assets/index/css/style.css';
-  .img1{
-    background-image: url(../../assets/index/images/img_bg_1.jpg);
-  }
-
-  .el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 150px;
-    margin: 0;
-  }
-
-  .el-carousel__item:nth-child(2n) {
-     background-color: #99a9bf;
-  }
-  
-  .el-carousel__item:nth-child(2n+1) {
-     background-color: #d3dce6;
-  }
 </style>
