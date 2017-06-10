157d1b77abb24962078eb4f5f65b491a5d830fd5
Ni_Chujie <ncj19991213@126.com>
2017-06-10 12:10:18 +0800

    Edit index

--- jpsp/frontend/src/components/index/JFooter.vue
+++ jpsp/frontend/src/components/index/JFooter.vue
@@ -4,17 +4,14 @@
     <div class="container">
       <div class="row row-pb-md">
         <div class="col-md-4 fh5co-widget">
-          <h4>Ink's</h4>
-          <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+          <h4>JPSP</h4>
+          <p>全称Jianping Student's Platform 。<br>是一个方便建平学生进行日常学校活动的平台，加强校内管理部门（如团委、学生会、自管会、社团部等）与学生的互动，利于发布和获取实时资讯。</p>
         </div>
         <div class="col-md-4 col-md-push-1">
           <h4>Links</h4>
           <ul class="fh5co-footer-links">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Practice Areas</a></li>
-            <li><a href="#">Won Cases</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">About us</a></li>
+            <li><a href="http://jianping.com.cn">建平中学</a></li>
+            <li><a href="">About</a></li>
           </ul>
         </div>
 
@@ -24,7 +21,7 @@
             <li>地址：浦东崮山路517号、385号(北校区)<br>电话：021-58851542</li>
             <li>传真：021-38821698</li>
             <li><a href="tel://1234567920">邮箱：jpzxxzyx2016@163.com</a></li>
-            <li><a href="mailto:info@yoursite.com">版权所有：Jeek</a></li>
+            <li><a href="mailto:info@yoursite.com">版权所有：Jeek信息社</a></li>
           </ul>
         </div>
 
--- jpsp/frontend/src/pages/index/index.vue
+++ jpsp/frontend/src/pages/index/index.vue
@@ -4,13 +4,13 @@
 <aside id="fh5co-hero" class="js-fullheight">
 		<div class="flexslider js-fullheight">
 			<ul class="slides">
-		   	<li style="background-image: url(images/img_bg_1.jpg);">
+		   	<li class="bi1">
 		   		<div class="overlay-gradient"></div>
 		   		<div class="container-fluid">
 		   			<div class="row">
 			   			<div class="col-md-6 col-md-offset-3 col-md-pull-3 js-fullheight slider-text slider-text-bg">
 			   				<div class="slider-text-inner">
-			   					<h1>Not Every Project Needs To Be Perfect</h1>
+			   					<h1>我们回来了</h1>
 									<h2>More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></h2>
 									<p class="ct"><a href="#">Learn More <i class="icon-arrow-right"></i></a></p>
 			   				</div>
@@ -18,7 +18,7 @@
 			   		</div>
 		   		</div>
 		   	</li>
-		   	<li style="background-image: url(images/img_bg_2.jpg);">
+		   	<li class="bi2">
 		   		<div class="overlay-gradient"></div>
 		   		<div class="container-fluid">
 		   			<div class="row">
@@ -32,7 +32,7 @@
 			   		</div>
 		   		</div>
 		   	</li>
-		   	<li style="background-image: url(images/img_bg_3.jpg);">
+		   	<li class="bi3">
 		   		<div class="overlay-gradient"></div>
 		   		<div class="container-fluids">
 		   			<div class="row">
@@ -124,8 +124,8 @@
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-          <h2>Our Co-League</h2>
-          <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+          <h2>Contributors</h2>
+          <p>我们的征途是星辰大海</p>
         </div>
       </div>
       <div class="row">
@@ -135,7 +135,47 @@
           </div>
           <div class="col-md-6 nopadding">
             <div class="desc">
-              <h3>John Doe</h3>
+              <h3>邱世贸 Alien Mao</h3>
+              <span>JEEK信息社 副社长</span>
+              <p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+              <p>
+                <ul class="fh5co-social-icons">
+                  <li><a href="#"><i class="icon-twitter-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-facebook-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-linkedin-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-dribbble-with-circle"></i></a></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="member">
+          <div class="col-md-6 nopadding col-md-push-6">
+            <div class="author" style="background-image: url(images/user-2.jpg);"></div>
+          </div>
+          <div class="col-md-6 nopadding col-md-pull-6">
+            <div class="desc">
+              <h3>倪楚劼 Cheney Ni</h3>
+              <span>JEEK信息社 社长</span>
+              <p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
+              <p>
+                <ul class="fh5co-social-icons">
+                  <li><a href="#"><i class="icon-twitter-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-facebook-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-linkedin-with-circle"></i></a></li>
+                  <li><a href="#"><i class="icon-dribbble-with-circle"></i></a></li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="member">
+          <div class="col-md-6 nopadding">
+            <div class="author" style="background-image: url(images/user-3.jpg);"></div>
+          </div>
+          <div class="col-md-6 nopadding">
+            <div class="desc">
+              <h3>邱龙田</h3>
               <span>CEO, Founder</span>
               <p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
               <p>
@@ -155,28 +195,8 @@
           </div>
           <div class="col-md-6 nopadding col-md-pull-6">
             <div class="desc">
-              <h3>John Doe</h3>
-              <span>CEO, Founder</span>
-              <p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
-              <p>
-                <ul class="fh5co-social-icons">
-                  <li><a href="#"><i class="icon-twitter-with-circle"></i></a></li>
-                  <li><a href="#"><i class="icon-facebook-with-circle"></i></a></li>
-                  <li><a href="#"><i class="icon-linkedin-with-circle"></i></a></li>
-                  <li><a href="#"><i class="icon-dribbble-with-circle"></i></a></li>
-                </ul>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="member">
-          <div class="col-md-6 nopadding">
-            <div class="author" style="background-image: url(images/user-3.jpg);"></div>
-          </div>
-          <div class="col-md-6 nopadding">
-            <div class="desc">
-              <h3>John Doe</h3>
-              <span>CEO, Founder</span>
+              <h3>倪楚劼 Cheney Ni</h3>
+              <span>JEEK信息社 社长</span>
               <p>Quos quia provident consequuntur culpa facere ratione maxime commodi voluptates id repellat velit eaque aspernatur expedita. Possimus itaque adipisci.</p>
               <p>
                 <ul class="fh5co-social-icons">
@@ -193,51 +213,13 @@
     </div>
   </div>
 
-
-  <div id="fh5co-blog">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-          <h2>Recent Post</h2>
-          <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-lg-6 col-md-6">
-          <div class="fh5co-blog">
-            <a href="#"><img class="img-responsive" src="images/blog-1.jpg" alt=""></a>
-            <div class="blog-text">
-              <span class="posted_on">Nov. 15th</span>
-              <span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-              <h3><a href="#">We've grown from a regional</a></h3>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div> 
-          </div>
-        </div>
-        <div class="col-lg-6 col-md-6">
-          <div class="fh5co-blog">
-            <a href="#"><img class="img-responsive" src="images/blog-2.jpg" alt=""></a>
-            <div class="blog-text">
-              <span class="posted_on">Nov. 15th</span>
-              <span class="comment"><a href="">21<i class="icon-speech-bubble"></i></a></span>
-              <h3><a href="#">We've grown from a regional</a></h3>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
-              <a href="#" class="btn btn-primary">Read More</a>
-            </div> 
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div id="fh5co-started" style="background-image:url(images/img_bg_2.jpg);">
+  <div id="fh5co-started" class="bi4">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
         <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
-          <h2>Want To Write About Us!</h2>
-          <p>Facilis ipsum reprehenderit nemo molestias. Aut cum mollitia reprehenderit. Eos cumque dicta adipisci architecto culpa amet.</p>
+          <h2>联系我们</h2>
+          <p>JPSP是一个由三个高中生建立并维护的网站项目，但由于时间、资金有限，我们需要更多有志之士的帮助。如果你觉得这个网站还不错，并且有维护网站的兴趣和决心，请联系我们！<br>我们欢迎任何人的加入！<br><br></p>
           <p><a href="#" class="btn btn-default btn-lg">Contact Us</a></p>
         </div>
       </div>
@@ -269,4 +251,21 @@
   @import '../../assets/index/css/magnific-popup.css';
   @import '../../assets/index/css/flexslider.css';
   @import '../../assets/index/css/style.css';
+
+  .bi1{
+    background-image: url(../../assets/6.jpg);
+  }
+
+  .bi2{
+    background-image: url(../../assets/4.jpg);
+  }
+
+  .bi3{
+    background-image: url(../../assets/2.jpg);
+  }
+
+  .bi4{
+    background-image: url(../../assets/5.jpg);
+  }
+  
 </style>
