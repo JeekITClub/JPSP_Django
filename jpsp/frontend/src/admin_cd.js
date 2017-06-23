8a1c51a54c16ec6c21ce2d5b911fbf5213abf8d0
qiushimao <qiushimao@hotmail.com>
2017-06-23 18:25:46 +0800

    [Entry] update admin_cd.js

--- jpsp/frontend/src/admin_cd.js
+++ jpsp/frontend/src/admin_cd.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import AdminCD from './AdminCD.vue'
-import router from './router'
+import router from './router/admin_cd'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import Vuex from 'vuex'
