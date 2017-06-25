c511ec46b6a6f18d0f90254120e61b0dc248e226
qiushimao <qiushimao@hotmail.com>
2017-06-25 13:20:21 +0800

    [Component] update admin_cd/PostList.vue

--- jpsp/frontend/src/pages/admin_cd/PostList.vue
+++ jpsp/frontend/src/pages/admin_cd/PostList.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <cd_post_list></cd_post_list>
+    <el-tabs v-model="editableTabsValue2" type="card" closable @tab-remove="removeTab">
+      <el-tab-pane label="excel" name="first">
+        <cd_post_list></cd_post_list>
+      </el-tab-pane>
+      <el-tab-pane label="adw" name="fasd">
+        <cd_post_list></cd_post_list>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 <script>
@@ -10,7 +17,46 @@
       'cd_post_list': PostList
     },
     data () {
-      return {}
+      return {
+        editableTabsValue2: 'first',
+        editableTabs2: [{
+          title: 'Tab 1',
+          name: '1',
+          content: ''
+        }, {
+          title: 'Tab 2',
+          name: '2',
+          content: 'Tab 2 content'
+        }],
+        tabIndex: 2
+      }
+    },
+    methods: {
+      addTab (targetName) {
+        let newTabName = ++this.tabIndex + ''
+        this.editableTabs2.push({
+          title: 'New Tab',
+          name: newTabName,
+          content: 'New Tab content'
+        })
+        this.editableTabsValue2 = newTabName
+      },
+      removeTab (targetName) {
+        let tabs = this.editableTabs2
+        let activeName = this.editableTabsValue2
+        if (activeName === targetName) {
+          tabs.forEach((tab, index) => {
+            if (tab.name === targetName) {
+              let nextTab = tabs[index + 1] || tabs[index - 1]
+              if (nextTab) {
+                activeName = nextTab.name
+              }
+            }
+          })
+        }
+        this.editableTabsValue2 = activeName
+        this.editableTabs2 = tabs.filter(tab => tab.name !== targetName)
+      }
     }
   }
 </script>
