9803f95ebdb7c5e97216ca31fe90e2e2824435fb
qiushimao <qiushimao@hotmail.com>
2017-08-15 11:10:38 +0800

    [FrontEnd] update
    fix styles

--- jpsp/frontend/src/pages/index/ClubShow.vue
+++ jpsp/frontend/src/pages/index/ClubShow.vue
@@ -7,26 +7,14 @@
       </div>
       </div>
       <div class="row">
-        <!-- TODO: 饿了么的分页毫无显示 -->
-        <nav aria-label="Page navigation">
-        <ul class="pagination">
-          <li>
-            <a href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li><a href="#">1</a></li>
-          <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
-          <li><a href="#">4</a></li>
-          <li><a href="#">5</a></li>
-          <li>
-            <a href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page.sync="currentPage1"
+          :page-size="100"
+          layout="total, prev, pager, next"
+          :total="1000" align="center">
+        </el-pagination>
       </div>
     </div>
   </div>
